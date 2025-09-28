5142a0a533c1829bd9cd7f90fc046bce4c2e27f0
sharviinamdar <sassyinamdar@gmail.com>
2025-09-28 16:56:23 -0700

    Update MyTestOpMode.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyTestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyTestOpMode.java
@@ -1,36 +1,64 @@
 package org.firstinspires.ftc.teamcode;
 
-import static com.qualcomm.robotcore.hardware.DcMotorSimple.Direction.FORWARD;
+import static com.qualcomm.robotcore.hardware.DcMotorSimple.Direction.REVERSE;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name="Drive Test")
 public class MyTestOpMode extends LinearOpMode {
-    final double MAX_SPEED = 1.0;
+    final double MAX_SPEED_41 = 1.0;
+    final double FEEDER_41 = 0.67;
+    final double SHOOTER_41 = 0.41;
     @Override
     public void runOpMode() throws InterruptedException {
-        DcMotor leftDrive = hardwareMap.dcMotor.get("leftDrive");
-        DcMotor rightDrive = hardwareMap.dcMotor.get("rightDrive");
+        DcMotor leftDrive_67 = hardwareMap.dcMotor.get("leftDrive");
+        DcMotor rightDrive_67 = hardwareMap.dcMotor.get("rightDrive");
+
+        CRServo feederRight_67 = hardwareMap.crservo.get("feederRight");
+        CRServo feederLeft_67 =  hardwareMap.crservo.get("feederLeft");
+
+        DcMotor shooter_67 = hardwareMap.dcMotor.get("shooter_67");
+
 
         waitForStart();
 
         if(isStopRequested()) return;
 
         while(opModeIsActive()) {
-            double leftInput = gamepad1.left_stick_y * MAX_SPEED;
-            double rightInput = -gamepad1.right_stick_y * MAX_SPEED;
+            double leftInput = -gamepad1.left_stick_y * MAX_SPEED_41;
+            double rightInput = -gamepad1.right_stick_y * MAX_SPEED_41;
 
-            leftDrive.setDirection(FORWARD);
-            leftDrive.setPower(leftInput);
-            rightDrive.setPower(rightInput);
+            leftDrive_67.setDirection(REVERSE);
+            leftDrive_67.setPower(leftInput);
+            rightDrive_67.setPower(rightInput);
+
+            if(gamepad2.y) {
+                feederRight_67.setPower(FEEDER_41);
+                feederLeft_67.setPower(-FEEDER_41);
+            }
+            else if (gamepad2.a) {
+                feederRight_67.setPower(-FEEDER_41);
+                feederLeft_67.setPower(FEEDER_41);
+
+            }
+            else{
+                feederRight_67.setPower(0);
+                feederLeft_67.setPower(0);
+            }
+            if(gamepad2.dpad_up){
+                shooter_67.setPower(SHOOTER_41);
+
+            }
+            else if(gamepad2.dpad_down){
+                shooter_67.setPower(-SHOOTER_41);
+            }
+            else{
+                shooter_67.setPower(0);
+            }
 
         }
-
-
-
-
-
     }
 }
